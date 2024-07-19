aa0ea6811909d55237eb0d05e214fe2bd667902d
Christian Berchtold <71659265+k0ruy@users.noreply.github.com>
2024-07-19 14:26:21 +0200

    Update lspconfig.lua, pyright import should work

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -13,51 +13,10 @@ lspconfig.wgsl_analyzer.setup({
 lspconfig.pyright.setup({
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "python" },
+  filetypes = {"python"},
   settings = {
-    python = {
-      analysis = {
-        useLibraryCodeForTypes = true,
-        diagnosticSeverityOverrides = {
-          reportGeneralTypeIssues = "none",
-          reportOptionalMemberAccess = "none",
-          reportOptionalSubscript = "none",
-          reportPrivateImportUsage = "none",
-          reportMissingModuleSource = false,
-        },
-        autoImportCompletions = false,
-      },
-      linting = { pylintEnabled = false }
-    }
-  },
+     python = {
+       pythonPath = "/home/ko4/miniforge3/envs/fplier/bin/python",
+     },
+   },
 })
-
-lspconfig.pylsp.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "python" },
-
-  settings = {
-    configurationSources = { "flake8" },
-    pylsp = {
-      plugins = {
-        -- jedi_completion = {fuzzy = true},
-        -- jedi_completion = {eager=true},
-        jedi_completion = {
-          include_params = true,
-        },
-        jedi_signature_help = { enabled = true },
-        pyflakes = { enabled = true },
-        -- pylint = {args = {'--ignore=E501,E231', '-'}, enabled=true, debounce=200},
-        -- pylsp_mypy={enabled=false},
-        pycodestyle = {
-          enabled = true,
-          ignore = { 'E501', 'E231', 'W503' },
-          maxLineLength = 120
-        },
-        yapf = { enabled = true }
-      }
-    }
-  }
-}
-)
